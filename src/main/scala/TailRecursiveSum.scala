88bb7165a1ce143809bd831e4051679c9f81736f
sandeshyapuram <sandeshyapuram@gmail.com>
2018-12-10 14:27:19 +0530

    q

---
+++ src/main/scala/TailRecursiveSum.scala
@@ -0,0 +1,9 @@
+class TailRecursiveSum extends App{
+  def sum(f: Int => Int)(a: Int, b: Int): Int = {
+    def loop(a: Int, acc: Int): Int = {
+      if (???) ???
+      else loop(???, ???)
+    }
+    loop(???, ???)
+  }
+}
